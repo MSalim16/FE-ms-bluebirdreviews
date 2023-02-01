479e83c1b0182f943f1bc7f327e0e3ea2f674406
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2023-02-01 21:53:45 +0000

    fixed twitter boxes

--- src/components/AllReviews.js
+++ src/components/AllReviews.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable no-unused-vars */
 import { getReviews } from "../api";
-import { useState, useEffect } from "react";
+import userContext from "../contexts/User";
+import { useState, useEffect, useContext } from "react";
 import { Link, useParams, useSearchParams } from "react-router-dom";
 import SortandOrderReviews from "./SortandOrderReviews";
 
@@ -8,6 +11,8 @@ const AllReviews = () => {
 
   const [isLoading, setIsLoading] = useState(true);
   const { category } = useParams();
+
+  const { user } = useContext(userContext);
 
   useEffect(() => {
     getReviews(category).then(reviewsFromApi => {
@@ -27,15 +32,54 @@ const AllReviews = () => {
           return (
             <li className="list" key={review.review_id}>
               <div className="item-card">
-                <h2 className="item-name">{review.title}</h2>
-                <img className="item-img" src={review.review_img_url}></img>
+                <div className="tweet">
+                  <Link to={`/reviews/${review.review_id}`}>
+                    <div className="twitter-handle">
+                      {review.owner === "cooljmessy" && (
+                        <img
+                          className="avatar-img"
+                          src="https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002"
+                        ></img>
+                      )}
+                      {review.owner === "tickle122" && (
+                        <img
+                          className="avatar-img"
+                          src="https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953"
+                        ></img>
+                      )}
+                      {review.owner === "jessjelly" && (
+                        <img
+                          className="avatar-img"
+                          src="https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141"
+                        ></img>
+                      )}
+                      {review.owner === "weegembump" && (
+                        <img
+                          className="avatar-img"
+                          src="https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553"
+                        ></img>
+                      )}
 
-                <Link to={`/reviews/${review.review_id}`}>
-                  <div className="read-more">
-                    <em>...</em>
-                    <strong>read more</strong>
-                  </div>
-                </Link>
+                      {review.owner === "grumpy19" && (
+                        <img
+                          className="avatar-img"
+                          src="https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013"
+                        ></img>
+                      )}
+
+                      {review.owner === "happyamy2016" && (
+                        <img
+                          className="avatar-img"
+                          src="https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729"
+                        ></img>
+                      )}
+
+                      <h1 className="author"> @{review.owner}</h1>
+                    </div>
+                    <h2 className="item-name">{review.title}</h2>
+                    <img className="item-img" src={review.review_img_url}></img>
+                  </Link>
+                </div>
               </div>
             </li>
           );
